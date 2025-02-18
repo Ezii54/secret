7ae9cce33947200061d22840861b2dc50ffdebbc
Ezii54 <alunheadshoter@gmail.com>
2025-02-18 13:24:43 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const Cormorant = Cormorant_Garamond({
+    variable: "--font-cormorant-sans",
     subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+    weight: "300",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-color-md text-white`}
+                className={`${Cormorant.variable} antialiased bg-color-md text-white`}
             >
                 <div className="p-4">{children}</div>
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,12 @@ import React, { useEffect, useState } from "react";
 import Login from "./comp/Login";
 import Loading from "./comp/Loading";
 import TypeIt from "typeit-react";
-import Link from "next/link";
 
 const page = () => {
-    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isLoggedIn, setLoggedIn] = useState(true);
 
     const handleLoginSuccess = () => {
         setLoggedIn(true);
-    };
-
-    const handleLogout = () => {
-        setLoggedIn(false);
     };
 
     const [isLoading, setLoading] = useState(true);
@@ -22,14 +17,6 @@ const page = () => {
     useEffect(() => {
         setTimeout(() => setLoading(false), 2000);
     }, []);
-
-    const [isConfess, setConfess] = useState(false);
-
-    const handleConfes = () => {
-        setTimeout(() => {
-            setConfess(true);
-        }, 2000);
-    };
 
     return (
         <>
@@ -44,24 +31,17 @@ const page = () => {
                     ) : (
                         <section className="flex flex-col gap-4">
                             <div className="p-2 text-sm rounded-lg shadow-inner bg-color-lg shadow-color-xl">
+                                <div className="text-center">
+                                    It's dedicated to my girl
+                                </div>
                                 <TypeIt
                                     options={{
                                         speed: 80,
                                         cursor: false,
-                                        afterComplete: () => {
-                                            handleConfes();
-                                        },
                                     }}
                                     getBeforeInit={(instance) => {
                                         instance
                                             .pause(3000)
-                                            .type(
-                                                "- It's dedicated to my girl -",
-                                                {
-                                                    delay: 1000,
-                                                }
-                                            )
-                                            .break({ delay: 500 })
                                             .break({ delay: 500 })
                                             .type(
                                                 "Di hari yang istimewa ini..",
@@ -96,27 +76,7 @@ const page = () => {
                                                 }
                                             )
                                             .type(
-                                                ", dan dewasa dalam menghadapi sesuatu.",
-                                                {
-                                                    delay: 1000,
-                                                }
-                                            )
-                                            .break({ delay: 500 })
-                                            .break({ delay: 500 })
-                                            .type(
-                                                "Ini hari dimana kamu harus melangkah",
-                                                {
-                                                    delay: 1000,
-                                                }
-                                            )
-                                            .type(
-                                                ", kayuh kapalmu dimana pun kamu berada",
-                                                {
-                                                    delay: 1000,
-                                                }
-                                            )
-                                            .type(
-                                                ", karena aku sedang menunggumu diujung Dermaga.",
+                                                ", dan dewasa dalam menghadapi segala situasi yang terjadi.",
                                                 {
                                                     delay: 1000,
                                                 }
@@ -146,35 +106,23 @@ const page = () => {
                                             )
                                             .break({ delay: 500 })
                                             .break({ delay: 500 })
-                                            .type("- Ezii", {
-                                                delay: 1000,
-                                            });
+                                            .type(
+                                                `Happy Birthday Intan Ayu Amalia`,
+                                                {
+                                                    delay: 1000,
+                                                }
+                                            )
+                                            .break({ delay: 500 })
+                                            .type(
+                                                "18 Februari 2025, by: @Mhnxzy",
+                                                {
+                                                    delay: 1000,
+                                                }
+                                            );
                                         return instance;
                                     }}
                                 />
                             </div>
-
-                            {isConfess ? (
-                                <div className="flex flex-col items-center justify-center gap-2 p-2 text-sm rounded-lg shadow-inner bg-color-lg shadow-color-xl">
-                                    Ingin membalas pesan ini?
-                                    <div className="flex items-center justify-center w-full gap-2">
-                                        <button
-                                            onClick={handleLogout}
-                                            className="w-1/2 rounded-md bg-color-red"
-                                        >
-                                            Tidak
-                                        </button>
-                                        <Link
-                                            passHref
-                                            target="_blank"
-                                            href={"https://wa.me/628872979779"}
-                                            className="w-1/2 text-center rounded-md bg-color-green"
-                                        >
-                                            Balas
-                                        </Link>
-                                    </div>
-                                </div>
-                            ) : null}
                         </section>
                     )}
                 </>
